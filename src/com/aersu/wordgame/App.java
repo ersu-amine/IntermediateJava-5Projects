e2d6dee00d9d1cab42def0511ae367209969b375
ersu-amine <ersu-amine@gmail.com>
2024-12-11 23:59:15 -0500

    completed word guessing game with implementation of more than 1 round of game

--- src/com/aersu/wordgame/App.java
+++ src/com/aersu/wordgame/App.java
@@ -1,9 +1,33 @@
 package com.aersu.wordgame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         String [] words = {"coffee","tea","milk","juice","soup","water"};
-        WordGuessingGame game = new WordGuessingGame(words,9);
-        game.play();
+
+        Scanner input = new Scanner(System.in);
+        String answer;
+        System.out.println("Let's play a game.");
+        do{
+            WordGuessingGame game = new WordGuessingGame(words,9);
+            //play game and get guessed word if won the game
+            String word = game.play();
+            //TODO implement removal of guessed word
+            System.out.println(word);
+            words = WordGuessingGame.removeGuessedWord(words,word);
+            System.out.println("Arrays.toString(words) = " + Arrays.toString(words));
+
+            //ask for second game
+            System.out.print("Would you like to play another game? yes or no: ");
+            answer = input.nextLine();
+
+        }while(answer.toLowerCase().equals("yes"));
+
     }
+
+
 }
--- src/com/aersu/wordgame/WordGuessingGame.java
+++ src/com/aersu/wordgame/WordGuessingGame.java
@@ -13,7 +13,6 @@ public class WordGuessingGame {
         //selecting random word from words array to be guessed
         Random random = new Random();
         this.wordToGuess = words[random.nextInt(words.length)];
-        System.out.println(wordToGuess);
 
         //initializing guessed letters array after the selection of word
         guessedLetters = new char[this.wordToGuess.length()];
@@ -21,22 +20,30 @@ public class WordGuessingGame {
         Arrays.fill(guessedLetters, '_');
     }
 
-    public void play() {
+    public String play() {
         Scanner input = new Scanner(System.in);
+        boolean gameWon = false;
+        String result = "";
 
         while (attempts > 0) {
+            displayGameState();
             System.out.println("You have " + attempts + " remaining attempts");
             System.out.println("Make a guess: ");
             char letter = input.nextLine().toLowerCase().charAt(0);
             if (processGuess(letter)) {
-                System.out.println("You have a guess!");
-                System.out.println(Arrays.toString(guessedLetters));
+                if (new String(guessedLetters).equals(wordToGuess)) {
+                    System.out.println("You have guessed all letters. You won.");
+                    //record the guessed word so it can be removed from array to prevent double game with same word
+                    return wordToGuess;
+                }
             } else {
                 System.out.println("Try again :) ");
             }
             attempts--;
         }
 
+        System.out.println("The word was " + wordToGuess + ". You are out of guesses. \nYou lose.");
+        return result;
     }
 
     private boolean checkLetter(char letter) {
@@ -59,5 +66,24 @@ public class WordGuessingGame {
             correctGuess = true;
         }
         return correctGuess;
+    }
+
+    //display letter
+    private void displayGameState() {
+        System.out.println("Guessed letters are: " + new String(guessedLetters));
+    }
+
+    public static String[] removeGuessedWord(String[] array, String word) {
+        System.out.println("Arrays.toString(words) = " + Arrays.toString(array));
+        String[] modifiedArray = new String[array.length - 1];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!array[i].equals(word)) {
+                array[i] = modifiedArray[index];
+                index++;
+            }
+
+        }
+        return modifiedArray;
     }
 }
