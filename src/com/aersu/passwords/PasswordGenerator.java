4da624bdb3c1c4bab3b56fb4f67b272b1202301a
ersu-amine <ersu-amine@gmail.com>
2024-12-14 01:13:59 -0500

    add if statements to check boolean arguments for type of characters the password should contain implement loop to fill password up to the requested length

--- src/com/aersu/passwords/PasswordGenerator.java
+++ src/com/aersu/passwords/PasswordGenerator.java
@@ -14,10 +14,38 @@ public class PasswordGenerator {
         Random random = new Random();
         //to store generated password
         StringBuilder password = new StringBuilder();
+        StringBuilder allChars = new StringBuilder();
 
-        for (int i = 0; i < length; i++) {
+        if(useLowercase){
+            //add 1 letter from lowercase letter set
             password.append(LOWERCASE.charAt(random.nextInt(LOWERCASE.length())));
+            allChars.append(LOWERCASE);
+        }
 
+        if(useUppercase){
+            //add 1 letter from uppercase letter set
+            password.append(UPPERCASE.charAt(random.nextInt(UPPERCASE.length())));
+            allChars.append(UPPERCASE);
+        }
+
+        if(useSpecialChar){
+            //add 1 letter from special character set
+            password.append(SPECIALCHARS.charAt(random.nextInt(SPECIALCHARS.length())));
+            allChars.append(DIGITS);
+        }
+
+        if(useNum){
+            //add 1 letter from digit set
+            password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+            allChars.append(DIGITS);
+        }
+
+        //check if password reached the desired length, iterate and add characters to password
+        if (password.length()<length){
+            for (int i = 0; i < length-password.length(); i++) {
+                //add a character from pool of characters that user requested
+                password.append(allChars.charAt(random.nextInt(allChars.length())));
+            }
         }
 
         //return String.valueOf(password);
